d8005c03899d6e08cf144d7666607cc7354fad98
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2018-10-12 05:32:18 +0300

    Changeset to plugins / configs

--- babel.config.js
+++ babel.config.js
@@ -4,16 +4,25 @@ module.exports = function(api) {
 	api.cache(true);
 	
 	const presets = [ '@babel/preset-env', '@babel/preset-react' ];
-	const env = { "production": { "presets": ["react-optimize"] }};
+	const env = { 'production': { 'presets': ['react-optimize'] }};
 
 	const plugins = [
 		['@babel/plugin-proposal-decorators', { 'legacy': true }],
 		['@babel/plugin-proposal-class-properties'],
 		['@babel/plugin-proposal-optional-chaining'],
 		['@babel/plugin-proposal-nullish-coalescing-operator'],
+		['@babel/plugin-syntax-dynamic-import'],
 		['@babel/plugin-transform-async-to-generator'],
 		['@babel/plugin-transform-flow-strip-types'],
-		['@babel/plugin-transform-react-display-name']
+		['@babel/plugin-transform-modules-commonjs'],
+		['@babel/plugin-transform-react-display-name'],
+		['module-resolver', {
+		  'root': ['./src'],
+		  'alias': {
+			'tests': './tests',
+			'routes': './routes'
+		  }
+		}]
 	];
 	const comments = false;
 	
